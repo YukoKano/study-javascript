a8f659ac19649fa2d9f44bb29bc1e7e47ae678ba
YukoKano <halloween_1031@hotmail.co.jp>
2023-03-13 23:52:32 +0900

    tab切り替え実装

--- 01_tab/src/javascript/script.js
+++ 01_tab/src/javascript/script.js
@@ -1,3 +1,27 @@
 // console.log(document.getElementsByClassName("hiragana"));
 console.log(document.getElementById("kakikukeko"));
 // console.log(document.querySelector("#id"));
+
+window.onload = () => {
+  // console.log(document.querySelectorAll('[data-js="list"]'));
+  const list = document.querySelectorAll(".list");
+  // console.log(list);
+
+  list.forEach(value => {
+    // console.log(value);
+    value.addEventListener("click", () => {
+      // console.log(value.dataset.js);
+      document.querySelectorAll(".content").forEach(i => {
+        i.classList.remove("is-active");
+      });
+
+      document.querySelectorAll(".list").forEach(i => {
+        i.classList.remove("is-active");
+      });
+
+      document.querySelector('[data-content="' + value.dataset.js + '"]').classList.add("is-active");
+
+      document.querySelector('[data-js="' + value.dataset.js + '"]').classList.add("is-active");
+    });
+  });
+}
