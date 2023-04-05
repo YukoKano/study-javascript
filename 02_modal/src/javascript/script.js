c0ac8681db38e95e8b346858a43bc7d2b0592409
YukoKano <halloween_1031@hotmail.co.jp>
2023-04-05 23:00:44 +0900

    modalレビュー対応

--- 02_modal/src/javascript/script.js
+++ 02_modal/src/javascript/script.js
@@ -1,118 +1,95 @@
 const activeClassName = "is-active";
+const imagePath = "./src/images/";
 
 window.onload = () => {
   const body = document.body;
 
-  const modalWrap = document.querySelector(".modalWrap");
-  const modalImage = document.querySelector(".modalImage");
-  const modalTitle = document.querySelector(".modalTitle");
-  const modalDate = document.querySelector(".modalDate");
-  const modalText = document.querySelector(".modalText");
+  const modalElement = {
+    wrap: document.querySelector(".modalWrap"),
+    image: document.querySelector(".modalImage"),
+    title: document.querySelector(".modalTitle"),
+    date: document.querySelector(".modalDate"),
+    text: document.querySelector(".modalText")
+  }
 
   const closeItems = document.querySelectorAll(".closeItem");
 
   const imageBox = document.querySelector('.imageBox');
   const imageArray = [
     {
-      src: "./src/images/01.JPG",
-      width: 200,
-      height: 200,
+      src: "01.JPG",
       title: "空",
       date: "2018-04-00",
       text: "hoge"
     },
     {
-      src: "./src/images/02.JPG",
-      width: 200,
-      height: 200,
+      src: "02.JPG",
       title: "桜と空",
       date: "2018-04-00",
       text: "雲ひとつない青空、4月、桜が咲き乱れている。「桜の樹の下には死体が埋まっている」と大学のサークルで誰かが自慢げに話していたが、くだらない。「桜が埋められたのは土壌をよりかたく頑丈にするためだ」という話の方が僕は好きだ。だから川沿いに桜が埋められている。そう、どう考えても、死体なんか埋まっていない。雲ひとつない青空、4月、桜が咲き乱れている。「桜の樹の下には死体が埋まっている」と大学のサークルで誰かが自慢げに話していたが、くだらない。「桜が埋められたのは土壌をよりかたく頑丈にするためだ」という話の方が僕は好きだ。だから川沿いに桜が埋められている。そう、どう考えても、死体なんか埋まっていない。雲ひとつない青空、4月、桜が咲き乱れている。「桜の樹の下には死体が埋まっている」と大学のサークルで誰かが自慢げに話していたが、くだらない。「桜が埋められたのは土壌をよりかたく頑丈にするためだ」という話の方が僕は好きだ。だから川沿いに桜が埋められている。そう、どう考えても、死体なんか埋まっていない。雲ひとつない青空、4月、桜が咲き乱れている。「桜の樹の下には死体が埋まっている」と大学のサークルで誰かが自慢げに話していたが、くだらない。「桜が埋められたのは土壌をよりかたく頑丈にするためだ」という話の方が僕は好きだ。だから川沿いに桜が埋められている。そう、どう考えても、死体なんか埋まっていない。"
     },
     {
-      src: "./src/images/03.JPG",
-      width: 200,
-      height: 200,
+      src: "03.JPG",
       title: "桜",
       date: "2018-04-00",
       text: "hello"
     },
     {
-      src: "./src/images/04.JPG",
-      width: 200,
-      height: 200,
+      src: "04.JPG",
       title: "満開の桜",
       date: "2018-04-00",
       text: "bloom"
     },
     {
-      src: "./src/images/05.JPG",
-      width: 200,
-      height: 200,
+      src: "05.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/06.JPG",
-      width: 200,
-      height: 200,
+      src: "06.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/07.JPG",
-      width: 200,
-      height: 200,
+      src: "07.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/08.JPG",
-      width: 200,
-      height: 200,
+      src: "08.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/09.JPG",
-      width: 200,
-      height: 200,
+      src: "09.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/10.JPG",
-      width: 200,
-      height: 200,
+      src: "10.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/11.JPG",
-      width: 200,
-      height: 200,
+      src: "11.JPG",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/12.jpg",
-      width: 200,
-      height: 200,
+      src: "12.jpg",
       title: "test",
       date: "2018-05-20",
       text: "test"
     },
     {
-      src: "./src/images/13.jpg",
-      width: 200,
-      height: 200,
+      src: "13.jpg",
       title: "test",
       date: "2018-05-20",
       text: "test"
@@ -121,62 +98,69 @@ window.onload = () => {
 
   const addImages = () => {
     // 配列にある画像を子要素に追加する
-    imageArray.forEach((image) => {
+    imageArray.forEach((image, index) => {
+      let list = document.createElement('li');
       let content = document.createElement('img');
-      content.src = image.src;
-      content.width = image.width;
-      content.height = image.height;
+      content.src = `${imagePath + image.src}`;
+      content.width = 200;
+      content.height = 200;
       content.alt = image.title;
-      imageBox.appendChild(content);
+      content.dataset.number = index;
+      list.appendChild(content);
+      imageBox.appendChild(list);
     });
+  }
+
+  const toggleOverlay = (status) => {
+    if (status === "fixed") {
+      // overlay部分を動かさないようにする
+      let scrollY = window.scrollY;
+      body.style.top = `-${scrollY}px`;
+      body.style.position = "fixed";
+    } else if (status === "unfixed") {
+      // overlay部分を動かせるようにする
+      let beforePositionY = body.style.top.replace("px", "");
+      body.style.top = '';
+      body.style.position = '';
+      window.scrollTo(0, beforePositionY * -1);
+    }
   }
 
   const createModal = (item) => {
     // arrayからsrcが一致するデータを探す
-    // urlの末尾の/をなくす
-    const url = location.href.replace(/(.+)\/$/, (_, p1) => p1);
-    const obj = imageArray.find(e => e.src === item.src.replace(url, "."));
+    const obj = imageArray[`${item.dataset.number}`];
 
     // modalにデータを入れる
-    modalImage.src = obj.src;
-    modalImage.alt = obj.title;
-    modalImage.width = obj.width;
-    modalImage.height = obj.height;
-    modalTitle.appendChild(document.createTextNode(obj.title));
-    modalDate.appendChild(document.createTextNode(obj.date));
-    modalText.appendChild(document.createTextNode(obj.text));
-
-    // overlay部分を動かさないようにする
-    let scrollY = window.scrollY;
-    body.style.top = `-${scrollY}px`;
-    body.style.position = "fixed";
+    modalElement.image.src = `${imagePath + obj.src}`;
+    modalElement.image.alt = obj.title;
+    modalElement.image.width = obj.width;
+    modalElement.image.height = obj.height;
+    modalElement.title.appendChild(document.createTextNode(obj.title));
+    modalElement.date.appendChild(document.createTextNode(obj.date));
+    modalElement.text.appendChild(document.createTextNode(obj.text));
 
     // modalを表示する
-    modalWrap.classList.add(activeClassName);
+    modalElement.wrap.classList.add(activeClassName);
   }
 
   const deleteModal = () => {
     // modal内のテキストを削除する（imgは上書きされるから不要）
-    modalTitle.removeChild(modalTitle.firstChild);
-    modalDate.removeChild(modalDate.firstChild);
-    modalText.removeChild(modalText.firstChild);
-
-    // overlay部分を動かせるようにする
-    let beforePositionY = body.style.top.replace("px", "");
-    body.style.top = '';
-    body.style.position = '';
-    window.scrollTo(0, beforePositionY * -1);
+    modalElement.title.removeChild(modalElement.title.firstChild);
+    modalElement.date.removeChild(modalElement.date.firstChild);
+    modalElement.text.removeChild(modalElement.text.firstChild);
 
     // modalを非表示にする
-    modalWrap.classList.remove(activeClassName);
+    modalElement.wrap.classList.remove(activeClassName);
   }
 
-  const changeModal = (item, status)  => {
+  const displayModal = (item, status)  => {
     item.addEventListener("click", () => {
       if (status === "open") {
         createModal(item);
+        toggleOverlay("fixed");
       } else if (status === "close") {
         deleteModal();
+        toggleOverlay("unfixed");
       }
     });
   }
@@ -187,11 +171,11 @@ window.onload = () => {
   // 画像を押すとモーダルが開く
   const images = document.querySelectorAll('.imageBox img'); // addImages()してからやらないと空っぽのためここに記載
   images.forEach(image => {
-    changeModal(image, "open");
+    displayModal(image, "open");
   });
 
   // overlayと閉じるボタンを押すとモーダルが閉じる
   closeItems.forEach(item => {
-    changeModal(item, "close");
+    displayModal(item, "close");
   });
 }
