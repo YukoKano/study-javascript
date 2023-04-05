03c87a14febdc37eba1a285cd76849f15fda4422
YukoKano <halloween_1031@hotmail.co.jp>
2023-04-05 23:53:12 +0900

    inputと追加ボタンの作成

--- 03_todoApp/src/javascript/script.js
+++ 03_todoApp/src/javascript/script.js
@@ -1,3 +1,18 @@
 window.onload = () => {
-  console.log("hoge");
+  const todoContent = document.getElementById("todoContent");
+  const todoLists = document.querySelector(".todoLists");
+
+  todoContent.addEventListener('change', () => {
+    const list = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('name', 'finished');
+    list.appendChild(checkbox);
+
+    list.appendChild(document.createTextNode(todoContent.value));
+
+    todoLists.appendChild(list);
+    todoContent.value = "";
+  })
 }
