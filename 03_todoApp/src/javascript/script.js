7df04b066ea3b7a6840157e9da20a6d5f40edc7d
YukoKano <halloween_1031@hotmail.co.jp>
2023-04-20 23:09:13 +0900

    [todoApp]createTodoの中の関数を外に出す

--- 03_todoApp/src/javascript/script.js
+++ 03_todoApp/src/javascript/script.js
@@ -32,82 +32,80 @@ window.onload = () => {
     })
   };
 
+  const addCheckbox = (item, index, listItem) => {
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', `tab${index}`);
+    checkbox.classList.add('checkbox');
+
+    checkbox.addEventListener('change', (item) => {
+      const list = item.target.parentNode;
+      const listNum = list.dataset.list;
+
+      if (item.target.checked) {
+        todoLists[listNum].isChecked = true;
+        list.classList.add('finished');
+        setTimeout((i) => { i.target.parentNode.classList.add('hide'); }, 1000, item);
+      } else {
+        todoLists[listNum].isChecked = false;
+        list.classList.remove('finished');
+      }
+
+      localStorage.setItem('todoLists', JSON.stringify(todoLists));
+    })
+
+    if (item.isChecked) {
+      listItem.classList.add('finished', 'hide');
+      checkbox.checked = true;
+    }
+
+    listItem.appendChild(checkbox);
+  }
+
+  const addLabel = (item, index, listItem) => {
+    const label = document.createElement('label');
+    label.setAttribute('for', `tab${index}`);
+    label.appendChild(document.createTextNode(item.content));
+    listItem.appendChild(label);
+
+    if (item.due != '') {
+      const due = document.createElement('span');
+      due.appendChild(document.createTextNode(item.due));
+
+      checkOverdue(item.due) && due.classList.add('overdue');
+      listItem.appendChild(due);
+    }
+  }
+
+  const addDeleteButton = (listItem) => {
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.classList.add('deleteButton');
+    button.addEventListener('click', (item) => {
+      const list = item.target.parentNode;
+      const listNum = list.dataset.list;
+      todoLists[listNum].isDeleted = true;
+      localStorage.setItem('todoLists', JSON.stringify(todoLists));
+
+      const displayNone = () => {
+        list.style.display = 'none';
+      }
+
+      // 消したものは絶対表示しない
+      todoLists[listNum].isDeleted && displayNone();
+    })
+
+    listItem.appendChild(button);
+  }
+
   const createTodo = (item, index) => {
-    // setup -------------------------------------
-    const addCheckbox = () => {
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('id', `tab${index}`);
-      checkbox.classList.add('checkbox');
-
-      checkbox.addEventListener('change', (item) => {
-        const list = item.target.parentNode;
-        const listNum = list.dataset.list;
-
-        if (item.target.checked) {
-          todoLists[listNum].isChecked = true;
-          list.classList.add('finished');
-          setTimeout((i) => { i.target.parentNode.classList.add('hide'); }, 1000, item);
-        } else {
-          todoLists[listNum].isChecked = false;
-          list.classList.remove('finished');
-        }
-
-        localStorage.setItem('todoLists', JSON.stringify(todoLists));
-      })
-
-      if (item.isChecked) {
-        list.classList.add('finished', 'hide');
-        checkbox.checked = true;
-      }
-
-      list.appendChild(checkbox);
-    }
-
-    const addLabel = () => {
-      const label = document.createElement('label');
-      label.setAttribute('for', `tab${index}`);
-      label.appendChild(document.createTextNode(item.content));
-      list.appendChild(label);
-
-      if (item.due != '') {
-        const due = document.createElement('span');
-        due.appendChild(document.createTextNode(item.due));
-
-        checkOverdue(item.due) && due.classList.add('overdue');
-        list.appendChild(due);
-      }
-    }
-
-    const addDeleteButton = () => {
-      const button = document.createElement('button');
-      button.setAttribute('type', 'button');
-      button.classList.add('deleteButton');
-      button.addEventListener('click', (item) => {
-        const list = item.target.parentNode;
-        const listNum = list.dataset.list;
-        todoLists[listNum].isDeleted = true;
-        localStorage.setItem('todoLists', JSON.stringify(todoLists));
-
-        const displayNone = () => {
-          list.style.display = 'none';
-        }
-
-        // 消したものは絶対表示しない
-        todoLists[listNum].isDeleted && displayNone();
-      })
-
-      list.appendChild(button);
-    }
-
-    // main create list ---------------------------------------
     const list = document.createElement('li');
     list.setAttribute('data-list', index);
     list.classList.add('todoList');
 
-    addCheckbox();
-    addLabel();
-    addDeleteButton();
+    addCheckbox(item,index, list);
+    addLabel(item, index, list);
+    addDeleteButton(list);
 
     todoListBox.appendChild(list);
   }
